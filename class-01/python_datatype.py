f06f865013a2d436c9c5e5a68164d9d414ff4836
smsamrat <s.m.samrat1215@gmail.com>
2024-06-04 21:44:09 +0600

    add class 1 folder

---
+++ class-01/python_datatype.py
@@ -0,0 +1,15 @@
+# a = 'I am bangladeshi' # string
+
+# b = 10 # integet
+
+# c = 10.5 # float
+
+# d = True # bool
+
+# e = False #bool
+
+# f = None # faka
+
+# print(type(b))
+
+a = 10
---
+++ class-01/variable_python.py
@@ -0,0 +1,8 @@
+belal123 = 10
+ap1234pel = 50
+
+_abc = 10
+
+abc_ = 10
+
+rahim_khan = 100
